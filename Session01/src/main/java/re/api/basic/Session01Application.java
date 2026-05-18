861254e4522d413e207b51a8f5e71d5ec9a9fec6
cccc9126 <huyhuy9126@gmail.com>
2026-05-18 10:42:28 +0700

    ss3

---
+++ Session01/src/main/java/re/api/basic/Session01Application.java
@@ -0,0 +1,13 @@
+package re.api.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Session01Application.class, args);
+	}
+
+}
---
+++ Session01/src/test/java/re/api/basic/Session01ApplicationTests.java
@@ -0,0 +1,13 @@
+package re.api.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
